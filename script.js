75c760a5cf128184c50c17a1b97b411b90f719c1
prosanjeet1998 <prosanjeet@innovatiqconsulting.com>
2024-05-12 10:12:28 +0530

    added the index and script file and updaded the mynewjs file

---
+++ script.js
@@ -0,0 +1,8 @@
+function funarea() {
+  console.log(
+    "This is the fun area for updating the git branch i love to edit it"
+  );
+
+  console.log(20 + 30);
+  console.log("sanny" + "shubham");
+}
