56ceb4957fe1a23630c088ab41c651d9f43c88d3
prosanjeet1998 <prosanjeet@innovatiqconsulting.com>
2024-05-12 10:58:24 +0530

    this is the branch2

--- mynnew.js
+++ mynnew.js
@@ -1,3 +1,6 @@
 function myfun() {
   console.log("hello prosanjeet");
+  console.log("this is the javascript file");
+  console.log("this is the marged commend ");
+  console.log("i want to learn hoe to update the git brancha");
 }
